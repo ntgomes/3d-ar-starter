a46c3c5100ee100278e0228d854197cba1cdf606
ntgomes <ntgomes@ncsu.edu>
2023-04-19 20:59:46 -0400

    Adjusting AddContent exercise

--- Assets/Scripts/AddContent/PlaceCube.cs
+++ Assets/Scripts/AddContent/PlaceCube.cs
@@ -6,30 +6,25 @@ public class PlaceCube : MonoBehaviour
 {
     public GameObject cubeToPlace;
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) { // If I press left click on my mouse...
-            
+        if (Input.GetMouseButtonDown(0)) { // Did I press left click on my mouse...
+            // If so, then...
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Create a line from the camera to where the mouse is
+            RaycastHit hit = new RaycastHit(); // Used for checking if the line touches something
+
+            if (Physics.Raycast (ray, out hit)) { // Does my line touch something where the mouse was? Like a plane?
+                // If so, then...
+                
+            }
+
         }
     }
 }
 
 
-///////
-// CODE BLOCK BANK
-//
-// Use the below lines of code to help complete this script!
-// 
-// 1. Generate the cube to place:
-//        Instantiate(cubeToPlace, new Vector3(hit.point.x, hit.point.y + 1.0f, hit.point.z), Quaternion.identity);
-// 
-// 2. Cast a line from the camera to the mouse's click, used to indicate where the mouse clicked:
-//        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-//
-// 3. If the line drawn from the camera to the mouse's click touches something
-//        if (Physics.Raycast (ray, out hit)) { }
-//
-// 4. Needed in order to check if a line from a camera touches something
-//        RaycastHit hit = new RaycastHit();
-//////
+/*
+  TODO: Copy and paste the below line of code inside the right place on the code above!
+  Instantiate(cubeToPlace, new Vector3(hit.point.x, hit.point.y + 1.0f, hit.point.z), Quaternion.identity);
+*/
