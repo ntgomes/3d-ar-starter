1d0b24ff01a1a39cdd66c720b519b5f9cc4710f2
ntgomes <ntgomes@ncsu.edu>
2023-04-20 01:22:33 -0400

    Adjust Interaction exercise

--- Assets/Scripts/Interaction/InteractCube.cs
+++ Assets/Scripts/Interaction/InteractCube.cs
@@ -12,51 +12,64 @@ public class InteractCube : MonoBehaviour
 
     private RaycastHit hit;
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) // If I press left click on my mouse...
         {
+            // TODO: Something from the code block bank should go under here...
+
+
             if (Physics.Raycast (ray, out hit)) // If the line from the camera to the mouse touched something on the scene...
             {
                 if (hit.collider.gameObject.tag.Equals("Ground")) {
-                    // TODO: Your code - Deactivate the interactingObj if there is one -> turn back on its gravity and then set it to null
+                    // Deactivate the interactingObj if there is one -> turn back on its gravity and then set it to null
+                    if (interactingObj) {
+                        interactingObj.useGravity = true;
+                    }
+                    interactingObj = null;
 
-                    // TODO: Your code - Then create the cubeToPlace at the mouse's position (hint: see PlaceCube.cs for how this is done)
+                    // TODO: Create the cubeToPlace at the mouse's position (hint: the same code you copy-pasted from PlaceCube.cs)
+
+
                 } else if (hit.collider.gameObject.tag.Equals("Interactable") && interactingObj == null) // If you clicked on an "Interactable" object
                 {
                     interactingObj = hit.collider.gameObject.GetComponent<Rigidbody>(); // Set the interactingObject to the object you clicked on
                 }
             } else 
             {
-                // TODO: Your code - Deactivate the interactingObj if there is one -> turn back on its gravity and then set it to null
+                if (interactingObj) {
+                    interactingObj.useGravity = true;
+                }
+                interactingObj = null;
             }
         }
         
+        // TODO: Something from the code block bank should go under here...
 
 
-        // Bonus: What if you're right-clicking?
-        //    https://docs.unity3d.com/ScriptReference/Input.GetMouseButtonDown.html
+
+        /*
+         Bonus if you have time: What if you're right-clicking?
+          https://docs.unity3d.com/ScriptReference/Input.GetMouseButtonDown.html
+        */
+
     }
 }
 
-///////
-// CODE BLOCK BANK
-//
-// Use the below lines of code to help complete this script!
-// 
-// 1. Get the cube to move to where the mouse is if it's being interacted with:
-//      if (interactingObj) 
-//      {
-//          interactingObj.useGravity = false;
-//          interactingObj.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
-//      }
-// 
-// 2. Cast a line from the camera to the mouse's click, used to indicate where the mouse clicked and if that line touches something:
-//        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-//        hit = new RaycastHit();
-//
-// 3. If the line drawn from the camera to the mouse's click touches something
-//        if (Physics.Raycast (ray, out hit)) { }
-//
-//////
+/*
+ CODE BLOCK BANK
+
+ Use the below blocks of code to help complete this script!
+ 
+ 1. Get the cube to move to where the mouse is if it's being interacted with:
+      if (interactingObj) 
+      {
+          interactingObj.useGravity = false;
+          interactingObj.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+      }
+ 
+ 2. Cast a line from the camera to the mouse's click, used to indicate where the mouse clicked and if that line touches something:
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        hit = new RaycastHit();
+
+*/
